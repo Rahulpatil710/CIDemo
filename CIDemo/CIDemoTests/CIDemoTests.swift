1a29be450cb3f1e3c448eea4f2aa1edcf8176074
Rahul Patil <rpatil@mobifyi.com>
2019-12-29 01:52:55 +0530

    feat: Added Unit Test cases and verified.

--- CIDemo/CIDemoTests/CIDemoTests.swift
+++ CIDemo/CIDemoTests/CIDemoTests.swift
@@ -11,8 +11,19 @@ import XCTest
 
 class CIDemoTests: XCTestCase {
 
+    var email1: String?
+    var password1: String?
+    
+    var email2: String?
+    var password2: String?
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        email1 = "abc"
+        password1 = "123"
+        
+        email2 = "abc@abc.com"
+        password2 = "ABC@abc123"
     }
 
     override func tearDown() {
@@ -22,6 +33,14 @@ class CIDemoTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        if let email = email1, let password = password1 {
+            XCTAssertFalse(email.isValidEmail(), "Email1 is not valid email")
+            XCTAssertFalse(password.isValidPassword(), "Password1 is not valid strong password")
+        }
+        if let email = email2, let password = password2 {
+            XCTAssertTrue(email.isValidEmail(), "Email1 is valid email")
+            XCTAssertTrue(password.isValidPassword(), "Password1 is valid strong password")
+        }
     }
 
     func testPerformanceExample() {
