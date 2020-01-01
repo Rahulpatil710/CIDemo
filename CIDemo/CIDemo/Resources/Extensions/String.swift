3b062b2d45589e220a07655d4de84fc23ac3a57b
Rahul Patil <rpatil@mobifyi.com>
2020-01-02 01:40:47 +0530

    refactor:update regx expression for email and password

--- CIDemo/CIDemo/Resources/Extensions/String.swift
+++ CIDemo/CIDemo/Resources/Extensions/String.swift
@@ -8,21 +8,25 @@
 
 import Foundation
 
-let __firstpart = "[A-Z0-9a-z]([A-Z0-9a-z._%+-]{0,30}[A-Z0-9a-z])?"
-let __serverpart = "([A-Z0-9a-z]([A-Z0-9a-z-]{0,30}[A-Z0-9a-z])?\\.){1,5}"
-let __emailRegex = __firstpart + "@" + __serverpart + "[A-Za-z]{2,8}"
-let __emailPredicate = NSPredicate(format: "SELF MATCHES %@", __emailRegex)
+//let __firstpart = "[A-Z0-9a-z]([A-Z0-9a-z._%+-]{0,30}[A-Z0-9a-z])?"
+//let __serverpart = "([A-Z0-9a-z]([A-Z0-9a-z-]{0,30}[A-Z0-9a-z])?\\.){1,5}"
+//let __emailRegex = __firstpart + "@" + __serverpart + "[A-Za-z]{2,8}"
+//let __emailPredicate = NSPredicate(format: "SELF MATCHES %@", __emailRegex)
 
 extension String {
     
     /// Check string is valid email or not
     public func isValidEmail() -> Bool {
+        let __firstpart = "[A-Z0-9a-z]([A-Z0-9a-z._%+-]{0,30}[A-Z0-9a-z])?"
+        let __serverpart = "([A-Z0-9a-z]([A-Z0-9a-z-]{0,30}[A-Z0-9a-z])?\\.){1,5}"
+        let __emailRegex = __firstpart + "@" + __serverpart + "[A-Za-z]{2,8}"
+        let __emailPredicate = NSPredicate(format: "SELF MATCHES %@", __emailRegex)
         return __emailPredicate.evaluate(with: self)
     }
     
     /// Check string is valid strong password or not
     public func isValidPassword() -> Bool {
-        let passwordRegex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*()\\-_=+{}|?>.<,:;~`’]{7,10}$"
+        let passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&#])[A-Za-z\\d$@$!%*?&#]{7,}"
         return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: self)
     }
 }
